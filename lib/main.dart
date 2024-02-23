2756f6c2ed29afeedfc705cd841a7dc7a7bbef8a
Postch <50191722+KennedyCreative@users.noreply.github.com>
2024-02-24 05:55:00 +0900

    Project Don added 6 images code for random number from 1 to 6 is ready

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Dice(),
+  ));
+}
+
+class Dice extends StatefulWidget {
+  const Dice({Key? key}) : super(key: key);
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int randomNumber = 1;
+  List<int> historyNumber = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      body: Stack(
+        children: [
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  Random random = Random();
+                  int randomNumber = random.nextInt(6) + 1;
+                  if (historyNumber.length >= 6) {
+                    historyNumber.removeAt(0);
+                  }
+                  historyNumber.add(randomNumber);
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Image.asset(
+                  'images/dice${randomNumber.toString()}.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 10,
+            bottom: 0,
+            child: SizedBox(
+              height: 200,
+              width: 30,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: historyNumber.reversed.map((number) {
+                  return Image.asset(
+                    'images/dice$number.png',
+                  );
+                }).toList(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
